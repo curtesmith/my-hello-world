6e0dd1c9c258b0251ac8cff70ea9cd7f53f57ae2
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-24 23:06:49 -0400

    refactor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#set -o errexit -o nounset
 
 cd terraform
 terraform init
@@ -7,23 +6,3 @@ terraform init
 terraform plan -out=main.plan
 
 terraform apply main.plan
-
-# echo "Setting git user name"
-# #git config user.name curtesmith
-
-# echo "Setting git user email"
-# #git config user.email curt.e.smith@gmail.com
-
-# echo "Adding git upstream remote"
-# git remote add upstream "https://github.com/curtesmith/my-hello-world.git"
-
-# git checkout master
-
-# git add .
-
-# NOW=$(TZ=America/New_York date)
-
-# git commit -m "tfstate: $NOW [ci skip]"
-
-# echo "Pushing changes to upstream master"
-# git push upstream master
