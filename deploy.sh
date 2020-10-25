60a4d378555b9137f97f3a8cbd9b094677d6f06c
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 12:22:25 -0400

    debug kubectl

--- deploy.sh
+++ deploy.sh
@@ -35,5 +35,7 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.7.
 echo "region:" $(terraform output region)
 echo "cluster_name:" $(terraform output cluster_name)
 aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
+kubectl config view
+
 wget -O v0.3.6.tar.gz https://codeload.github.com/kubernetes-sigs/metrics-server/tar.gz/v0.3.6 && tar -xzf v0.3.6.tar.gz
 kubectl apply -f metrics-server-0.3.6/deploy/1.8+/
