30a7e55f02d80762e437090b61afcc0f05e0f8a4
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 08:31:07 -0400

    parallelize

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+#install terraform
+
 curl -sLo /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip
 unzip /tmp/terraform.zip -d /tmp
 mv /tmp/terraform ~/bin
 export PATH="~/bin:$PATH"
 
+
+#install awscli
 pyenv global 3.7.1
 pip install -U pip
 pip install awscli
 
+#retreive latest tfstate
 aws s3 cp s3://travis-builds-curtesmith/latest/terraform.tfstate terraform
 
+#apply infrastructue updates from main.tf
 cd terraform
 terraform init
 
@@ -17,6 +23,7 @@ terraform plan -out=main.plan
 
 terraform apply main.plan
 
+#retain terraform.tfstate
 cd ..
 
 aws s3 cp terraform/terraform.tfstate s3://travis-builds-curtesmith/latest/
