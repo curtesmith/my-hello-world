efb4ad9087d79ae7b59939ca36db63275676ef37
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 00:13:44 -0400

    changes

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ terraform plan -out=main.plan
 
 terraform apply main.plan
 
-aws s3 sync terraform.tfstate s3://travis-builds-curtesmith/$TRAVIS_BUILD_NUMBER
+cd ..
+aws s3 sync terraform s3://travis-builds-curtesmith/$TRAVIS_BUILD_NUMBER
