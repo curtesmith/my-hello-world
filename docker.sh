c5ff23253c1099ce30c89250c9e450cea54b73ca
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 08:54:44 -0400

    add comments

--- docker.sh
+++ docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# build the image
 docker build -t curtesmith/my-hello-world:latest .
+
+# authenticate to docker hub
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+
+# push the image to docker hub
 docker push curtesmith/my-hello-world:latest
