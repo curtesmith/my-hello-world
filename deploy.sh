f2e9b455a2204af25e7e3ec00d8a0d69f3965188
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-24 22:57:29 -0400

    remove git commands

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,22 @@ terraform plan -out=main.plan
 
 terraform apply main.plan
 
-echo "Setting git user name"
-#git config user.name curtesmith
+# echo "Setting git user name"
+# #git config user.name curtesmith
 
-echo "Setting git user email"
-#git config user.email curt.e.smith@gmail.com
+# echo "Setting git user email"
+# #git config user.email curt.e.smith@gmail.com
 
-echo "Adding git upstream remote"
-git remote add upstream "https://github.com/curtesmith/my-hello-world.git"
+# echo "Adding git upstream remote"
+# git remote add upstream "https://github.com/curtesmith/my-hello-world.git"
 
-git checkout master
+# git checkout master
 
-git add .
+# git add .
 
-NOW=$(TZ=America/New_York date)
+# NOW=$(TZ=America/New_York date)
 
-git commit -m "tfstate: $NOW [ci skip]"
+# git commit -m "tfstate: $NOW [ci skip]"
 
-echo "Pushing changes to upstream master"
-git push upstream master
+# echo "Pushing changes to upstream master"
+# git push upstream master
