38f3288888148e82cae0596cf7a3dfd51c03ebb7
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-24 23:57:45 -0400

    aws stuff

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+pyenv global 3.7.1
+pip install -U pip
+pip install awscli
+
+aws s3 sync s3://travis-builds-curtesmith terraform
 
 cd terraform
 terraform init
-cp ~/terraform.tfstate .
 
 terraform plan -out=main.plan
 
 terraform apply main.plan
+
+aws s3 sync terraform.tfstate s3://travis-builds-curtesmith/$TRAVIS_BUILD_NUMBER
