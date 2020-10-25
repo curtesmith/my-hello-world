9d83af30831d98a7e050ee0b843701ab4f4820f0
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-24 22:13:46 -0400

    added steps to save terraform state

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,23 @@ cd terraform
 terraform plan -out=main.plan
 
 terraform apply main.plan
+
+echo "Setting git user name"
+git config user.name $GH_USER_NAME
+
+echo "Setting git user email"
+git config user.email $GH_USER_EMAIL
+
+echo "Adding git upstream remote"
+git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
+
+git checkout master
+
+git add .
+
+NOW=$(TZ=America/New_York date)
+
+git commit -m "tfstate: $NOW [ci skip]"
+
+echo "Pushing changes to upstream master"
+git push upstream master
