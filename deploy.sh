9593cabb16f25495025fc04c1e57434fef282bd6
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 16:22:10 -0400

    install aws-iam-authenticator 2

--- deploy.sh
+++ deploy.sh
@@ -55,7 +55,8 @@ aws sts get-caller-identity
 #install AWS IAM Authenticator
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.8/2020-09-18/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
-mkdir -p /usr/local/bin && cp ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+mkdir -p /usr/local/bin && sudo cp ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+aws-iam-authenticator help
 
 echo "*** testing get svc ***"
 kubectl get svc
