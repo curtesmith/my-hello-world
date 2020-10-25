5b1934b5fc8cd34fd98da48efb4683fea709895d
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 09:14:42 -0400

    docker syntax

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t curtesmith/my-hello-world:latest .
-cho $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker push curtesmith/my-hello-world:latest
