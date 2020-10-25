b38346682d19d1649682469d5440eebbafdf1e96
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 09:09:01 -0400

    reverting

--- docker.sh
+++ docker.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# build the image
 docker build -t curtesmith/my-hello-world:latest .
-
-# authenticate to docker hub
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-
-# push the image to docker hub
+cho $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker push curtesmith/my-hello-world:latest
