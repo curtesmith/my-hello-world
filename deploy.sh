ce5b987aa9f91023aa4099053c17783aa851d581
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 22:12:18 -0400

    refactor

--- deploy.sh
+++ deploy.sh
@@ -34,39 +34,38 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 #    chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 #configure kubectl and deploy the metrics server
-echo "region:" $(terraform output region)
-echo "cluster_name:" $(terraform output cluster_name)
+# echo "region:" $(terraform output region)
+# echo "cluster_name:" $(terraform output cluster_name)
 aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
-echo "view configuration"
-kubectl config view
+# echo "view configuration"
+# kubectl config view
 
-echo "display config file"
-cat ~/.kube/config
+# echo "display config file"
+# cat ~/.kube/config
 
-echo "replace config"
-terraform output kubectl_config > ~/.kube/config
+# echo "replace config"
+# terraform output kubectl_config > ~/.kube/config
 
-echo "view updated configuration"
-kubectl config view
+# echo "view updated configuration"
+# kubectl config view
 
-echo "get identity"
-aws sts get-caller-identity
+# echo "get identity"
+# aws sts get-caller-identity
 
 #install AWS IAM Authenticator
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.8/2020-09-18/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mkdir -p /usr/local/bin && sudo cp ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
-aws-iam-authenticator help
+# aws-iam-authenticator help
 
-echo "*** testing get svc ***"
-kubectl get svc
+# echo "*** testing get svc ***"
+# kubectl get svc
 
 echo "apply metrics components"
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
 
 echo "apply dashboard"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
-
 
 echo "create the ClusterRoleBinding resource"
 kubectl apply -f https://raw.githubusercontent.com/hashicorp/learn-terraform-provision-eks-cluster/master/kubernetes-dashboard-admin.rbac.yaml
