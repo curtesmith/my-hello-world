3692932ac19d4ea1b8fe46b748cce1a1b3f81217
Curtis <curtis@Curtiss-MacBook-Pro.local>
2020-10-25 22:30:56 -0400

    refactor some more

--- prerequisites.sh
+++ prerequisites.sh
@@ -14,7 +14,16 @@ pip install awscli
 
 #retreive latest tfstate
 aws s3 cp s3://travis-builds-curtesmith/latest/terraform.tfstate terraform
+
+#apply infrastructue updates from main.tf
 cd terraform
+terraform init
+terraform plan -out=main.plan
+terraform apply main.plan
+
+#retain terraform.tfstate
+aws s3 cp terraform.tfstate s3://travis-builds-curtesmith/latest/
+aws s3 cp terraform.tfstate s3://travis-builds-curtesmith/archive/$TRAVIS_BUILD_NUMBER/
 
 #get kubectl and add to bin folder
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" && \
@@ -23,13 +32,21 @@ curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s ht
 #configure kubectl and deploy the metrics server
 aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
 
-cd ..
-
 #install AWS IAM Authenticator
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.8/2020-09-18/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mkdir -p /usr/local/bin && sudo cp ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 
+echo "apply metrics components"
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+
+echo "apply dashboard"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+
+echo "create the ClusterRoleBinding resource"
+kubectl apply -f https://raw.githubusercontent.com/hashicorp/learn-terraform-provision-eks-cluster/master/kubernetes-dashboard-admin.rbac.yaml
+
 echo "apply update for application and service"
+cd ..
 kubectl apply -f deploy.yml
 kubectl apply -f services.yml
